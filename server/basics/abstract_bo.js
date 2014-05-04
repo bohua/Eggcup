8a2aeabb07b0db31fe2a90c2084085a68a9c00f1
bohua <fantasybohua@gmail.com>
2014-05-05 00:00:21 +0200

    New changelist

---
+++ server/basics/abstract_bo.js
@@ -0,0 +1,122 @@
+/**
+ * Created by Bohua on 2014-05-03.
+ */
+var Q = require("q");
+var extend = require("extend");
+
+var BO = function (schema) {
+	this.orm = require(__dirname + "/../../models");
+	this._table = schema;
+
+	this.add = function (model) {
+		var deferred = Q.defer();
+
+		this.orm.model(this._table)
+			.build(model)
+			.save()
+			.then(function (success) {
+				deferred.resolve(success);
+			}, function (failure) {
+				deferred.reject(failure);
+			});
+
+		return deferred.promise;
+	};
+
+	this.update = function (model) {
+		var deferred = Q.defer();
+
+		this.orm.model(this._table).find({
+			where: {
+				id: model.id
+			}
+		}).then(function (success) {
+			if (success) {
+				success
+					.updateAttributes(model)
+					.then(function (success) {
+						deferred.resolve(success);
+					}, function (failure) {
+						deferred.reject(failure);
+					});
+			}
+		}, function (failure) {
+			deferred.reject(failure);
+		});
+
+		return deferred.promise;
+	};
+
+	this.delete = function (id) {
+		var deferred = Q.defer();
+
+		this.orm.model(this._table).find({
+			where: {
+				id: id
+			}
+		}).then(function (success) {
+			if (success) {
+				success
+					.destroy()
+					.then(function (success) {
+						deferred.resolve(success);
+					}, function (failure) {
+						deferred.reject(failure);
+					});
+			}
+		}, function (failure) {
+			deferred.reject(failure);
+		});
+
+		return deferred.promise;
+	};
+
+	this.get = function (id, enabledOnly) {
+		var deferred = Q.defer();
+
+		this.orm.model(this._table).find({
+			where: {
+				id: id,
+				enable: enabledOnly
+			}
+		}).then(
+			function (success) {
+				deferred.resolve(success);
+			},
+			function (failure) {
+				deferred.reject(failure);
+			});
+
+		return deferred.promise;
+	};
+
+	this.getAll = function (enabledOnly) {
+		var deferred = Q.defer();
+
+		this.orm.model(this._table).findAll({
+			where: {
+				enable: enabledOnly
+			}
+		}).then(
+			function (success) {
+				deferred.resolve(success);
+			},
+			function (failure) {
+				deferred.reject(failure);
+			});
+
+		return deferred.promise;
+	};
+
+	if (arguments.length > 1) {
+		for (var i in arguments) {
+			//Skip the first arg
+			if(i === "0"){
+				continue;
+			}
+			this[arguments[i].name] = arguments[i].method;
+		}
+	}
+}
+
+module.exports = BO;
