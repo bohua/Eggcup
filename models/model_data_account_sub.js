95bce616e1ced7227ae1a9773ab91104b8feefbb
bohua <fantasybohua@gmail.com>
2014-06-03 16:39:47 +0200

    new code

--- models/model_data_account_sub.js
+++ models/model_data_account_sub.js
@@ -10,7 +10,8 @@ module.exports = function (sequelize, DataTypes) {
 		 */
 		account_date:		{ type: DataTypes.DATE },
 		account_expense:	{ type: DataTypes.DECIMAL(16,2), defaultValue: 0.00 },
-		account_method:		{ type: DataTypes.STRING }
+		account_method:		{ type: DataTypes.STRING },
+		account_desc:		{ type: DataTypes.STRING }
 	}, {
 		classMethods: {
 			associate: function (models) {
