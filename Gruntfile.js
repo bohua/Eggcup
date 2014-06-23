a489181e502d2afd83997592aa34570fe763a797
bohua <fantasybohua@gmail.com>
2014-06-24 01:50:42 +0200

    update grunt file to auto packing

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,10 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>;\n' +
+			' * Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %>\n */\n',
+
 		// running `grunt less` will compile once
 		less: {
 			development: {
@@ -37,13 +42,38 @@ module.exports = function (grunt) {
 			files: [
 				"./public/**/*.less"//,
 				//"./public/**/*.jade"
-				],
+			],
 			tasks: ["less"]
+		},
+
+		build_dir: 'build',
+
+		clean: {
+			build: ['<%= build_dir %>/*']
+		},
+
+		copy: {
+			build: {
+				files: [
+					{
+						dest: '../build',
+						src: ['**', '!attachments/*.*', '!logs/*.* ', '!uploads/*.*', '!Gruntfile.js'],
+						expand: true
+					}
+				]
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-jade');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-html2js');
 
 	grunt.registerTask('default', ['less', 'jade', 'watch']);
+
+	grunt.registerTask('build', ['clean:build', 'copy:build']);
 };
