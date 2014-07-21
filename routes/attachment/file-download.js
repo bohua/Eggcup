019fa0e753539b7fcd1631ae24e02a42e52e6767
bohua <fantasybohua@gmail.com>
2014-07-21 10:45:30 +0200

    Fix bugs

--- routes/attachment/file-download.js
+++ routes/attachment/file-download.js
@@ -5,7 +5,8 @@ var path = require('path');
 
 module.exports = function (req, res) {
 	var file = req.params.fileUrl,
+		givenName = req.query.givenName,
 		filePath = path.normalize(__dirname + '/../../attachments/' + file);
 
-	res.download(filePath);
+	res.download(filePath, givenName);
 };
