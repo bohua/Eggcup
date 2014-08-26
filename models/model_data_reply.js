6172f61d79b87849e882694d083bdf38910568b0
bohua <fantasybohua@gmail.com>
2014-08-26 15:44:40 +0200

    Fix bug

--- models/model_data_reply.js
+++ models/model_data_reply.js
@@ -4,6 +4,29 @@
 module.exports = function (sequelize, DataTypes) {
 
 	var DATA_REPLY = sequelize.define('DATA_REPLY', {
+//		/**
+//		 * Reply Properties
+//		 */
+//		reply_withEmail:		{ type: DataTypes.BOOLEAN, defaultValue: false },
+//		reply_withTel:			{ type: DataTypes.BOOLEAN, defaultValue: false },
+//		reply_withF2F:			{ type: DataTypes.BOOLEAN, defaultValue: false },
+//		reply_withFax:			{ type: DataTypes.BOOLEAN, defaultValue: false },
+//		reply_date:				{ type: DataTypes.DATE },
+//		consult_person:			{ type: DataTypes.STRING },
+//		reply_person:			{ type: DataTypes.STRING},
+//		translate_person:		{ type: DataTypes.STRING},
+//
+//		/**
+//		 * F2F Properties
+//		 */
+//		meeting_address:		{ type: DataTypes.STRING },
+//		meeting_people_A:		{ type: DataTypes.STRING },
+//		meeting_people_B:		{ type: DataTypes.STRING },
+//
+//		consult_context:		{ type: DataTypes.TEXT },
+//		reply_context:			{ type: DataTypes.TEXT },
+//		law_context:			{ type: DataTypes.TEXT },
+
 		/**
 		 * Sheet Properties
 		 */
---
+++ upgrade_0_92_0_93.sql
@@ -0,0 +1,72 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS upgrade_database_0_92_0_93 $$
+CREATE PROCEDURE upgrade_database_0_92_0_93()
+BEGIN
+
+IF NOT EXISTS( (SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA='eggcup'
+        AND TABLE_NAME='data_reply_subs') ) THEN
+    CREATE TABLE data_reply_subs (
+      id int(11) NOT NULL AUTO_INCREMENT,
+      reply_withEmail tinyint(1) DEFAULT '0',
+      reply_withTel tinyint(1) DEFAULT '0',
+      reply_withF2F tinyint(1) DEFAULT '0',
+      reply_withFax tinyint(1) DEFAULT '0',
+      reply_date datetime DEFAULT NULL,
+      consult_person varchar(255) DEFAULT NULL,
+      reply_person varchar(255) DEFAULT NULL,
+      translate_person varchar(255) DEFAULT NULL,
+      meeting_address varchar(255) DEFAULT NULL,
+      meeting_people_A varchar(255) DEFAULT NULL,
+      meeting_people_B varchar(255) DEFAULT NULL,
+      consult_context text,
+      reply_context text,
+      law_context text,
+      createdAt datetime NOT NULL,
+      updatedAt datetime NOT NULL,
+      reply_id int(11) DEFAULT NULL,
+      PRIMARY KEY (id)
+    ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+    INSERT INTO data_reply_subs (
+        reply_withEmail,
+        reply_withTel,
+        reply_withF2F,
+        reply_withFax,
+        reply_date,
+        consult_person,
+        translate_person,
+        meeting_address,
+        meeting_people_A,
+        meeting_people_B,
+        consult_context,
+        reply_context,
+        law_context,
+        createdAt,
+        updatedAt,
+        reply_id
+    ) SELECT reply_withEmail,
+             reply_withTel,
+             reply_withF2F,
+             reply_withFax,
+             reply_date,
+             consult_person,
+             translate_person,
+             meeting_address,
+             meeting_people_A,
+             meeting_people_B,
+             consult_context,
+             reply_context,
+             law_context,
+             createdAt,
+             updatedAt,
+             id
+      FROM data_replies;
+
+
+
+END IF;
+
+END $$
+CALL upgrade_database_0_92_0_93() $$
+
+DELIMITER ;
