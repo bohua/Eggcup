075c3024a5f21c4d4d50d784424d7997c60cd039
bohua <fantasybohua@gmail.com>
2014-09-04 00:32:33 +0200

    fix bugs

--- public/src/partials/reply-editor/reply-editor-controller.js
+++ public/src/partials/reply-editor/reply-editor-controller.js
@@ -1,13 +1,14 @@
 /**
  * Created by Bohua on 2014-06-18.
  */
-angular.module('reply-editor', ['toggle-button-model', 'pop-confirm', 'employee-list-service', 'login-session-service', 'file-type-service'])
+angular.module('reply-editor', ['toggle-button-model', 'pop-confirm', 'employee-list-service', 'login-session-service', 'file-type-service', 'print-service'])
 	.controller('replyEditorController', [
 		'$scope',
 		'employeeListService',
 		'loginSessionService',
 		'fileTypeService',
-		function ($scope, employeeListService, loginSessionService, fileTypeService) {
+		'printService',
+		function ($scope, employeeListService, loginSessionService, fileTypeService, printService) {
 
 			$scope.employeeFieldOptions = {
 				source: _.pluck(employeeListService.getEmployeeList(), 'name')
@@ -65,4 +66,51 @@ angular.module('reply-editor', ['toggle-button-model', 'pop-confirm', 'employee-
 					$scope.Confirm('remove', true);
 				}
 			};
+
+			/**
+			 * Print
+			 */
+			$scope.printDoc = function () {
+				var sheetType = $scope.dialog_data_model.handling === 0 ? 'reply_a' : 'reply_b';
+				var consultMethods = [],
+					privileges = [];
+
+				if ($scope.dialog_data_model.prop_isEmail) consultMethods.push("电子邮件");
+				if ($scope.dialog_data_model.prop_isTel) consultMethods.push("电话");
+				if ($scope.dialog_data_model.prop_isF2F) consultMethods.push("面谈");
+				if ($scope.dialog_data_model.prop_isFax) consultMethods.push("传真");
+
+				if ($scope.dialog_data_model.prop_internal) privileges.push("内部浏览");
+				if ($scope.dialog_data_model.prop_external) privileges.push("客户浏览");
+
+				var params = {
+					sheetType: sheetType,
+					sheetData: {
+						singleMapper: {
+							customer_name: $scope.dialog_data_model.customer_name,
+							customer_contact: $scope.dialog_data_model.customer_contact,
+							customer_tel: $scope.dialog_data_model.customer_tel,
+							customer_email: $scope.dialog_data_model.customer_email,
+							customer_address: $scope.dialog_data_model.customer_address,
+							report_date: $scope.dialog_data_model.report_date.split('T')[0],
+
+							reply_date: $scope.dialog_data_model.reply_date.split('T')[0],
+
+							consult_context: $scope.dialog_data_model.consult_context,
+							reply_context: $scope.dialog_data_model.reply_context,
+							law_context: $scope.dialog_data_model.law_context,
+							reply_person: $scope.dialog_data_model.reply_person,
+							meeting_address: $scope.dialog_data_model.meeting_address,
+							meeting_people_A: $scope.dialog_data_model.meeting_people_A,
+							meeting_people_B: $scope.dialog_data_model.meeting_people_B,
+
+							consult_method: consultMethods.join(', '),
+							privilege: privileges.join(', ')
+						}
+					}
+				};
+
+				printService.print(params, '打印回复表_TK' + $scope.dialog_data_model.id);
+			};
+
 		}]);
--- public/src/partials/task-editor/3-reply-section/reply-section-controller.js
+++ public/src/partials/task-editor/3-reply-section/reply-section-controller.js
@@ -1,12 +1,11 @@
 /**
  * Created by Bli on 2014/6/16.
  */
-angular.module('reply-section', ['reply-editor', 'attach-editor', 'appointment-editor', 'print-service'])
+angular.module('reply-section', ['reply-editor', 'attach-editor', 'appointment-editor'])
 	.controller('replySectionController', [
 		'$scope',
 		'$timeout',
-		'printService',
-		function ($scope, $timeout, printService) {
+		function ($scope, $timeout) {
 
 			/**
 			 * Reply Editor Initialization
@@ -30,6 +29,12 @@ angular.module('reply-section', ['reply-editor', 'attach-editor', 'appointment-e
 				dataModel.handling = $scope.task_model.handling;
 				dataModel.customer_contact = $scope.task_model.customer_contact;
 				dataModel.inherit_consult_context = $scope.task_model.register_content;
+				dataModel.report_date = $scope.task_model.report_date;
+				dataModel.customer_name = $scope.task_model.customer_name;
+				dataModel.customer_contact = $scope.task_model.customer_contact;
+				dataModel.customer_tel = $scope.task_model.customer_tel;
+				dataModel.customer_email = $scope.task_model.customer_email;
+				dataModel.customer_address = $scope.task_model.customer_address;
 
 				$($event.currentTarget).trigger('popup', [mode || 'readOnly', dataModel]);
 			};
@@ -136,48 +141,4 @@ angular.module('reply-section', ['reply-editor', 'attach-editor', 'appointment-e
 				};
 				$scope.$emit('event:saveTaskModel', $scope.task_model.id, o);
 			};
-
-			$scope.printDoc = function () {
-				var sheetType = $scope.task_model.handling === 0 ? 'reply_a' : 'reply_b';
-				var consultMethods = [],
-					privileges = [];
-
-				if ($scope.task_model.prop_isEmail) consultMethods.push("电子邮件");
-				if ($scope.task_model.prop_isTel) consultMethods.push("电话");
-				if ($scope.task_model.prop_isF2F) consultMethods.push("面谈");
-				if ($scope.task_model.prop_isFax) consultMethods.push("传真");
-
-				if ($scope.task_model.prop_internal) privileges.push("内部浏览");
-				if ($scope.task_model.prop_external) privileges.push("客户浏览");
-
-				var params = {
-					sheetType: sheetType,
-					sheetData: {
-						singleMapper: {
-							customer_name: $scope.task_model.customer_name,
-							customer_contact: $scope.task_model.customer_contact,
-							customer_tel: $scope.task_model.customer_tel,
-							customer_email: $scope.task_model.customer_email,
-							customer_address: $scope.task_model.customer_address,
-							report_date: $scope.task_model.report_date.split('T')[0],
-
-							reply_date: $scope.task_model.replySheet.reply_date.split('T')[0],
-
-							consult_context: $scope.task_model.replySheet.consult_context,
-							reply_context: $scope.task_model.replySheet.reply_context,
-							law_context: $scope.task_model.replySheet.law_context,
-							reply_person: $scope.task_model.replySheet.reply_person,
-							meeting_address: $scope.task_model.replySheet.meeting_address,
-							meeting_people_A: $scope.task_model.replySheet.meeting_people_A,
-							meeting_people_B: $scope.task_model.replySheet.meeting_people_B,
-
-							consult_method: consultMethods.join(', '),
-							privilege: privileges.join(', ')
-						}
-					}
-				};
-
-				printService.print(params, '打印回复表_TK' + $scope.task_model.id);
-			};
-
 		}]);
